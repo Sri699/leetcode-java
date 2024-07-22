e87bd0cb39ab790218295fe4126b7e16847ffdc3
Sriram <144365989+Sri699@users.noreply.github.com>
2024-07-22 23:09:40 +0530

    Add files via upload

---
+++ Day1_SortThePeople.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String[] sortPeople(String[] names, int[] heights) {
+   HashMap<Integer,String> map = new HashMap<Integer,String>();
+       for(int i = 0;i<names.length;i++){
+        map.put(heights[i],names[i]);
+       }
+       Arrays.sort(heights);
+       String res[] = new String[heights.length];
+       int j =0;
+       for(int i = heights.length-1;i>=0;i--){
+        res[j] = map.get(heights[i]);
+        j++;
+       }
+       return res;
+    }
+}
